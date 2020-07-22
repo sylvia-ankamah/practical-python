d97da8bce0cbafb9e252c05c0b51c8b22edf3c49
Sylvia-ankamah <sankamah@mun.ca>
2020-07-21 23:29:43 -0230

    Exercise 2.11

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.7
+# Exercise 2.11
 import csv
 def read_portfolio(filename):
     '''Read in the holdings from a portfolio'''
@@ -45,4 +45,25 @@ for s in portfolio:
 print('Total cost:', total_cost)
 print('Current value:', current_value)
 print('Gain/Loss:', current_value - total_cost)
+
+def make_report_data(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+        
+report = make_report_data(portfolio, prices)
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    print('%10s %10d %10.2f %10.2f' % row)
     
