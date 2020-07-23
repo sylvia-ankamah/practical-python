43dc2b284faec84c3a11abd923693750fbac0c9c
Sylvia-ankamah <sankamah@mun.ca>
2020-07-23 03:27:37 -0230

    Exercise 3.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,14 +10,17 @@ def portfolio_cost(filename):
     portfolio = report.read_portfolio(filename)
     return sum([s['shares']*s['price'] for s in portfolio])
 
-import sys
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = input('Enter a filename:')
 
-cost = portfolio_cost(filename)
-print('Total cost:', cost)
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfoliofile' % args[0])
+    filename = args[1]
+    print('Total cost:', portfolio_cost(filename))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
+
 
 
 
