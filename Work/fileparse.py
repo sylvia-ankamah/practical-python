7bba16990a8f8d8156c95d55a765059a4cf720f0
Sylvia-ankamah <sankamah@mun.ca>
2020-07-23 02:30:40 -0230

    Exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     '''
     Parse a CSV file into a list of records
     '''
@@ -12,12 +12,25 @@ def parse_csv(filename):
 
         # Read the file headers
         headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for
+            colname in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
+            
+            if indices:
+                row = [ row[index] for index in
+                indices ]
+
+
             record = dict(zip(headers, row))
             records.append(record)
 
     return records
-    
